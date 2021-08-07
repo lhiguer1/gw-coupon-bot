59be48ddcc782436404a5b7ffac93c4b9b798ae0
Leonel Higuera <lhiguer1@asu.edu>
2021-08-07 02:13:19 -0700

    Upload saved coupon when command is called

--- cogs/gw_coupon.py
+++ cogs/gw_coupon.py
@@ -1,26 +1,28 @@
 import discord
+import os
+import calendar
+from datetime import datetime
 from discord.ext import commands
 
 class GWCoupon(commands.Cog):
     """Retrives Goodwill Coupons"""
     def __init__(self, bot):
         self.bot = bot
-    
-    async def cog_before_invoke(self, ctx):
-        await ctx.send(f'[DEBUG] cog_before_invoke')
-
-
-    async def cog_after_invoke(self, ctx):
-        await ctx.send(f'[DEBUG] cog_after_invoke')
 
     @commands.command(name='gw_coupon')
-    async def gw_coupon(self, ctx):
-        cat_pics = list()
-        cat_pics.append(discord.File(open('./cat0.jpg', 'rb'), filename='cat0.jpg'))
-        cat_pics.append(discord.File(open('./cat1.jpg', 'rb'), filename='cat1.jpg'))
+    async def gw_coupon(self, ctx: commands.Context):
+        today = datetime.today()
+        
+        coupon_png = discord.File(os.path.join('coupons', calendar.month_name[today.month].lower() + '.png'))
+        
+
+        # check if this months coupon is in the data structure.
+        # if not month in data structure
+        ## download coupon
+        # return coupn
 
         await ctx.send(f'De marca baby!')
-        await ctx.send(files=cat_pics)
+        await ctx.send(file=coupon_png)
 
 def setup(bot):
     bot.add_cog(GWCoupon(bot))
