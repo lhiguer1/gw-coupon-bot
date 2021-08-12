31b9f249b0b62b1d55f0adc8a7aa4e64fbdf02c5
Leonel Higuera <lhiguer1@asu.edu>
2021-08-12 12:57:24 -0700

    Tidying up

--- cogs/gw_coupon.py
+++ cogs/gw_coupon.py
@@ -12,17 +12,10 @@ class GWCoupon(commands.Cog):
     @commands.command(name='gw_coupon')
     async def gw_coupon(self, ctx: commands.Context):
         today = datetime.today()
-        
+
         coupon_png = discord.File(os.path.join('coupons', calendar.month_name[today.month].lower() + '.png'))
-        
 
-        # check if this months coupon is in the data structure.
-        # if not month in data structure
-        ## download coupon
-        # return coupn
-
-        await ctx.send(f'De marca baby!')
-        await ctx.send(file=coupon_png)
+        await ctx.send('De marca baby!', file=coupon_png)
 
 def setup(bot):
     bot.add_cog(GWCoupon(bot))
